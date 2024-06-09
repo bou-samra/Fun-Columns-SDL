c05d6fba64a1183e0389ef12c16d9c0b02a96227
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-06-09 19:45:51 +1000

    Update cluster_research.c

--- src/cluster_research.c
+++ src/cluster_research.c
@@ -155,18 +155,18 @@ int main (void) {
 	}
 // -- bottom left corner
 	z = 5;
-	for (k = 11; k < 16; k++) {
+	for (k = 6; k > 0; k--) {
 	for (i = 0; i < z; i++) {
 		counter = 1;
 						for (j = 0; j < 2; j++) {
-					if (grid[i + j + k][i + j] == grid[i + j + 1 + k][i + j + 1] && grid[i + j + 1 + k][i + j + 1] != 0) {
+					if (grid[k - i - j][i + j] == grid[k - i - j - 1][i + j + 1] && grid[k - i - j - 1][i + j + 1] != 0) {
 						counter = counter + 1;
 					}
 				}
 									if (counter == 3) {
 						printf("dne. match\n"/*add to cluster list*/);
 				} else {
-					printf("no dse. match\n");
+					printf("no dne. match\n");
 				}
 	}
 	z = z - 1;
