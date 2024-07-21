f0539e96ea9a23cb0e93cddea12411b882e61f78
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-07-21 18:36:46 +1000

    Update new_brick.c

--- src/new_brick.c
+++ src/new_brick.c
@@ -1,8 +1,8 @@
 // generate random 3 tile bricks for current and preview
 new_brick {
-	for (i=0; i<3; i++) {
-		tile_current[i] = randombytes_uniform(5)+1;	// current piece
-		tile_next[i] = randombytes_uniform(5)+1;	// next piece
+	for (i = 0; i < 3; i++) {
+		tile_current[i] = randombytes_uniform(5) + 1;	// current piece
+		tile_next[i] = randombytes_uniform(5) + 1;	// next piece
 	}
 	return 0;
 }
