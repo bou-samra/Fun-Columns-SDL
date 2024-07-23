77ced830107a042c3715b8348ba920f60a344d5a
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-07-23 19:08:07 +1000

    Update new_col.c

--- src/new_col.c
+++ src/new_col.c
@@ -20,3 +20,12 @@ int new_column() {
 		}
 	return actual_col;						// return the random column
 }
+
+
+//  display brick in relevant row/column of board array
+int disp_column(int row, int col)
+{
+	for (i = 0; i < 3; i++) {
+		grid[0][row + i][col] = tile_current[i];
+	}
+}
