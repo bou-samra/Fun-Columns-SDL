e1bb29bfb15f5348fb74493581c09a13ca14892a
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-08-03 10:31:00 +1000

    Update text.h

--- src/text.h
+++ src/text.h
@@ -14,6 +14,22 @@ char about[] = {	// about screen
 "FTA/TOOLBOX"
 };
 
+int aboutl [13][2] = {
+{118, 35},	// fun columns
+{118, 49},	// by fta
+{118, 63},	// coding:
+{118, 72},	// o. goguel
+{118, 88},	// design:
+{118, 97},	// o. bailly
+{118, 104},	//   maitre
+{118, 132},	// version 1.1
+{118, 141},	// toolbox-mag
+{118, 150},	// issue: 04
+{118, 159},	// 03-deb-91
+{118, 175},	// copyright
+{118, 184} 	// fta/toolbox
+};
+
 char high[] = {		//  high scores
 "HIGH   "
 "SCORES:"
@@ -33,59 +49,6 @@ char high[] = {		//  high scores
 "...4000"
 "DINO   "			// EMILE
 "......1"
-
-};
-
-char status[] = {	// current status
-"SCORE: "
-"      0"		// score
-"LEVEL: "
-"1E    1"		// levels remaining / completed (31)
-"TOTAL: "
-"      0"		// tiles smashed
-};
-
-char m_menu[] = {	// main menu
-"CHOOSE :   "
-"1-VERY EASY"
-"2-EASY     "
-"3-MEDIUM   "
-"4-HARD     "
-"5-VERY HARD"
-"6-JEDI     "
-"A-ABOUT    "
-"I-INFO     "
-"Q-QUIT     "
-};
-
-char info[] = {		// info/instruction screen
-"COLUMNS    "
-"RULES.     "
-" <-:LEFT   "
-" ->:RIGHT  "
-"  R:ROTATE "
-"SPC:FALL   "
-" ^R:RESTART"
-" ^S:NEW PAT"
-"  N:NEXT   "
-"  P:PAUSE  "
-" HAVE FUN !"
-};
-
-char gameo[] = {
-"GAME OVER"
-};
-
-char pause[] = {
-"> PAUSE <"
-};
-
-int gameol [1][2] = {
-{124, 97}
-};
-
-int pausel [1][2] = {
-{124, 97}
 };
 
 int highl [18][2] = {
@@ -109,20 +72,13 @@ int highl [18][2] = {
 {22, 182} 	// ......0
 };
 
-int aboutl [13][2] = {
-{118, 35},	// fun columns
-{118, 49},	// by fta
-{118, 63},	// coding:
-{118, 72},	// o. goguel
-{118, 88},	// design:
-{118, 97},	// o. bailly
-{118, 104},	//   maitre
-{118, 132},	// version 1.1
-{118, 141},	// toolbox-mag
-{118, 150},	// issue: 04
-{118, 159},	// 03-deb-91
-{118, 175},	// copyright
-{118, 184} 	// fta/toolbox
+char status[] = {	// current status
+"SCORE: "
+"      0"		// score
+"LEVEL: "
+"1E    1"		// levels remaining / completed (31)
+"TOTAL: "
+"      0"		// tiles smashed
 };
 
 int statusl [6][2] = {
@@ -132,6 +88,46 @@ int statusl [6][2] = {
 {240, 40},	// 00 1
 {240, 51},	// total:
 {240, 60} 	// 0
+};
+
+char m_menu[] = {	// main menu
+"CHOOSE :   "
+"1-VERY EASY"
+"2-EASY     "
+"3-MEDIUM   "
+"4-HARD     "
+"5-VERY HARD"
+"6-JEDI     "
+"A-ABOUT    "
+"I-INFO     "
+"Q-QUIT     "
+};
+
+int m_menul [10][2] =  {
+{118, 52},	// choose
+{118, 66},	// 1 - very easy
+{118, 73},	// 2 - easy
+{118, 80},	// 3 - medium
+{118, 87},	// 4 - hard
+{118, 94},	// 5 - very hard
+{118, 101},	// 6 - jedi
+{118, 115},	// a - about
+{118, 122},	// i - info
+{118, 129}	// q - quit
+};
+
+char info[] = {		// info/instruction screen
+"COLUMNS    "
+"RULES.     "
+" <-:LEFT   "
+" ->:RIGHT  "
+"  R:ROTATE "
+"SPC:FALL   "
+" ^R:RESTART"
+" ^S:NEW PAT"
+"  N:NEXT   "
+"  P:PAUSE  "
+" HAVE FUN !"
 };
 
 int infol [11][2] =  {
@@ -148,15 +144,18 @@ int infol [11][2] =  {
 {116, 134}	// have fun !
 };
 
-int m_menul [10][2] =  {
-{118, 52},	// choose
-{118, 66},	// 1 - very easy
-{118, 73},	// 2 - easy
-{118, 80},	// 3 - medium
-{118, 87},	// 4 - hard
-{118, 94},	// 5 - very hard
-{118, 101},	// 6 - jedi
-{118, 115},	// a - about
-{118, 122},	// i - info
-{118, 129}	// q - quit
+char gameo[] = {
+"GAME OVER"
+};
+
+int gameol [1][2] = {
+{124, 97}
+};
+
+char pause[] = {
+"> PAUSE <"
+};
+
+int pausel [1][2] = {
+{124, 97}
 };
