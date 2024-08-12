74d01b0b214e0fcafa15f532b9018fb08530186d
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-08-12 18:47:40 +1000

    Create events.h

---
+++ src/events.h
@@ -0,0 +1,12 @@
+#ifndef EVENTS_H
+#define EVENTS_H
+
+int handle_events ();
+
+extern bool next_p;	// next piece flag
+extern bool pause_f;
+extern bool quit;	// quit flag
+extern bool main_m;
+extern bool restart;
+
+#endif
