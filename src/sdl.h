88304ebfd78b034cf68c843a94ed1f450155b2f1
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-08-12 18:52:05 +1000

    Create sdl.h

---
+++ src/sdl.h
@@ -0,0 +1,17 @@
+#ifndef SDL_H
+#define SDL_H
+
+int initSDL ();
+int shutdownSDL ();
+int check_key(void);
+int init_gfx(void);
+
+// ============== SDL structures
+extern SDL_Window *window;
+extern SDL_Renderer *sr;
+extern SDL_Event event;
+extern SDL_RWops* rwops_new;
+extern SDL_Surface* image;
+extern SDL_Texture * texture;
+
+#endif
