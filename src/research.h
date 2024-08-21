e7941fde6fe29e6963d16d1ae37f31b8c128f45b
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-08-22 07:01:42 +1000

    Update research.h

--- src/research.h
+++ src/research.h
@@ -1,6 +1,7 @@
 #ifndef RESEARCH_H
 #define RESEARCH_H
 
-int research (void);		// cluster detection
+int research (void);		// detect clusters
+int eliminate_c (void);		// eliminate clusters
 
 #endif
