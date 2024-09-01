7cb3e22dc088ad11d12e41e935951f49ec2e6d0c
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-09-01 19:15:57 +1000

    Update research.h

--- src/research.h
+++ src/research.h
@@ -1,7 +1,9 @@
 #ifndef RESEARCH_H
 #define RESEARCH_H
 
-int research (void);		// detect clusters
+bool research (void);		// detect clusters
 int eliminate_c (void);		// eliminate clusters
+int cascade();			// cascade ceiling tiles
+int clear();			// clear matched array;
 
 #endif
