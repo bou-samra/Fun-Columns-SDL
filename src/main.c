3bfab4207dda2cc665b18f6ff28567eee8584ab0
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-09-10 19:28:45 +1000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -40,7 +40,14 @@ int menu(void) {
 				Ren_menu();
 			}
 			if (event.key.keysym.sym > SDLK_0 && event.key.keysym.sym < SDLK_7) {
+				if (event.key.keysym.sym == SDLK_1) {speed = 1.0;}
+					if (event.key.keysym.sym == SDLK_2) {speed = 1.5;}
+						if (event.key.keysym.sym == SDLK_3) {speed = 2.0;}
+							if (event.key.keysym.sym == SDLK_4) {speed = 2.5;}
+								if (event.key.keysym.sym == SDLK_5) {speed = 3.0;}
+									if (event.key.keysym.sym == SDLK_6) {speed = 3.5;}
 				handle_events();			// game levels
+				reset();
 				Ren_restart();
 				check_key();
 				restart = false;
