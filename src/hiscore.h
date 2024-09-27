77e7318ecadc0ad20fb6b9aa07623f5f83250647
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-09-28 09:46:13 +1000

    Update hiscore.h

--- src/hiscore.h
+++ src/hiscore.h
@@ -1,1 +1,16 @@
+#ifndef HISCORE_H
+#define HISCORE_H
 
+extern SDL_Rect gameo_back;
+extern SDL_Rect gameo_trim;	// game over trim
+extern SDL_Rect entern_back;	// enter name back ground
+extern SDL_Rect entern_trim;	// enter name trim
+extern SDL_Rect cursor;		// Name entry cursor
+
+int Ren_restart(void);		// restart
+int Ren_gameover(void);		// game over
+int Ren_name(void);		// render name entry
+int end_game(void);
+int Ren_cursor(void);		// render cursor
+
+#endif
