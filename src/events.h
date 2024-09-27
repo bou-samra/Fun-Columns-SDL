c23938eefcdb2d22ce19f218b87c72626d211ca3
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-09-28 09:44:04 +1000

    Update events.h

--- src/events.h
+++ src/events.h
@@ -6,7 +6,7 @@ int handle_events(void);
 extern bool next_p;	// next piece flag
 extern bool pause_f;
 extern bool quit;	// quit flag
-extern bool restart;
+extern int game_end;
 extern bool gradient_d;
 
 #endif
