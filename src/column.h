47e1f41ab450e4d159db0221e3ddf246b9986ba9
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-09-28 09:40:38 +1000

    Update column.h

--- src/column.h
+++ src/column.h
@@ -3,6 +3,7 @@
 
 extern int tile_current[3];
 extern int tile_next[3];
+extern int tile_initial[3];
 extern int grid[2][18][8];
 extern int mask[2];
 extern int spr_y[6];
@@ -10,7 +11,9 @@ extern int colcnt[8];
 
 int new_column(void);			// select random column to drop brick into
 int disp_column(int row, int col);	// update column board
-int new_brick(void);			// choose current/preview random brick
+int current_brick(void);			// choose current/preview random brick
+int next_brick(void);			// choose current/preview random brick
+int initial_brick(void);			// choose current/preview random brick
 int rotate_col(void);
 int display_tile(void);			// display current column in play (not implemented)
 int move_horiz(void);
