646dbdeb2f53c00a93564a3bad8b0355d88db2d5
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-10-09 20:09:38 +1100

    Update text.h

--- src/text.h
+++ src/text.h
@@ -2,23 +2,26 @@
 #define TEXT_H
 
 int Ren_line(char line[], int char_x, int char_y, int ln, int ll, bool col );
+void its(int, char *);
+void hts(char, char *);
 
 extern char about[];
-extern int aboutl [13][2];
+extern int aboutl [13][2];		// about text
 extern char high[];
-extern int highl [18][2];
+extern int highl [18][2];		// high score panel
 extern char status[];
-extern int statusl [6][2];
+extern int statusl [6][2];		// level panel
 extern char m_menu[];
-extern int m_menul [10][2];
+extern int m_menul [10][2];		// menu
 extern char info[];
-extern int infol [11][2];
+extern int infol [11][2];		// instructions
 extern char gameo[];
-extern int gameol [1][2];
+extern int gameol [1][2];		// game over / restart
 extern char pause[];
-extern int pausel [1][2];
+extern int pausel [1][2];		// pause
 extern char name[];
-extern int namel [2][2];
+extern int namel [2][2];		// end (enter name) prompt
+extern char newh[];
+extern int newhl [1][2];		// end (enter name) dialog box
 extern int ascii [128][4];
-
 #endif
