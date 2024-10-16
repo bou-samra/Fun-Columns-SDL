0bed2027ea92c924588b5b3a53442bb4302a1a9e
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-10-16 20:11:41 +1100

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -29,12 +29,12 @@ int menu(void) {
 				Ren_menu();
 			}
 			if (event.key.keysym.sym > SDLK_0 && event.key.keysym.sym < SDLK_7) {
-				if (event.key.keysym.sym == SDLK_1) {speed = 1.0;}
-					if (event.key.keysym.sym == SDLK_2) {speed = 1.5;}
-						if (event.key.keysym.sym == SDLK_3) {speed = 2.0;}
-							if (event.key.keysym.sym == SDLK_4) {speed = 2.5;}
-								if (event.key.keysym.sym == SDLK_5) {speed = 3.0;}
-									if (event.key.keysym.sym == SDLK_6) {speed = 3.5;}
+				if (event.key.keysym.sym == SDLK_1) {speed = 1.0; level_r = 0x1E; level_c = 1;}
+					if (event.key.keysym.sym == SDLK_2) {speed = 1.5;  level_r = 0x19; level_c = 5;}
+						if (event.key.keysym.sym == SDLK_3) {speed = 2.0;  level_r = 0x14; level_c = 10;}
+							if (event.key.keysym.sym == SDLK_4) {speed = 2.5;  level_r = 0x0F; level_c = 15;}
+								if (event.key.keysym.sym == SDLK_5) {speed = 3.0;  level_r = 0x0A; level_c = 20;}
+									if (event.key.keysym.sym == SDLK_6) {speed = 3.5;  level_r = 0x05; level_c = 25;}
 				handle_events();			// game levels
 				end_game();
 				Ren_menu();
