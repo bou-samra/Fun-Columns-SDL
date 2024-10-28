3514e4b6929c188b020a3e1ccc85a17137be6da5
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-10-28 20:16:26 +1100

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -4,6 +4,7 @@
 #include "events.h"
 #include "hiscore.h"
 #include "sdl.h"
+#include "level.h"
 
 ///////////////// MAIN MENU ////////////////////////////
 int menu(void) {
@@ -29,12 +30,12 @@ int menu(void) {
 				Ren_menu();
 			}
 			if (event.key.keysym.sym > SDLK_0 && event.key.keysym.sym < SDLK_7) {
-				if (event.key.keysym.sym == SDLK_1) {speed = 1.0; level_r = 0x1E; level_c = 1;}
-					if (event.key.keysym.sym == SDLK_2) {speed = 1.5;  level_r = 0x19; level_c = 5;}
-						if (event.key.keysym.sym == SDLK_3) {speed = 2.0;  level_r = 0x14; level_c = 10;}
-							if (event.key.keysym.sym == SDLK_4) {speed = 2.5;  level_r = 0x0F; level_c = 15;}
-								if (event.key.keysym.sym == SDLK_5) {speed = 3.0;  level_r = 0x0A; level_c = 20;}
-									if (event.key.keysym.sym == SDLK_6) {speed = 3.5;  level_r = 0x05; level_c = 25;}
+				if (event.key.keysym.sym == SDLK_1) {speed = levels[0][3]; level_r = levels[0][0]; level_c = levels[0][1];}
+					if (event.key.keysym.sym == SDLK_2) {speed = levels[5][3]; level_r = levels[5][0]; level_c = levels[5][1];}
+						if (event.key.keysym.sym == SDLK_3) {speed = levels[10][3]; level_r = levels[10][0]; level_c = levels[10][1];}
+							if (event.key.keysym.sym == SDLK_4) {speed = levels[15][3]; level_r = levels[15][0]; level_c = levels[15][1];}
+								if (event.key.keysym.sym == SDLK_5) {speed = levels[20][3]; level_r = levels[20][0]; level_c = levels[20][1];}
+									if (event.key.keysym.sym == SDLK_6) {speed = levels[25][3]; level_r = levels[25][0]; level_c = levels[25][1];}
 				handle_events();			// game levels
 				end_game();
 				Ren_menu();
