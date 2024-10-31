ebd2e18c4cc1873e87336cfd877d0333fe9b0b0e
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-10-31 19:01:37 +1100

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -5,14 +5,16 @@
 #include "hiscore.h"
 #include "sdl.h"
 #include "level.h"
+#include "column.h"
 
 ///////////////// MAIN MENU ////////////////////////////
 int menu(void) {
-	quit = false;
+		reset();
+//	quit = false;
 	Ren_menu();
 
 	while (quit == false) {
-//		SDL_Delay(20);
+		SDL_Delay(20);
 		SDL_PollEvent(&event);
 		switch (event.type)  {
 			case SDL_KEYDOWN:
@@ -30,12 +32,12 @@ int menu(void) {
 				Ren_menu();
 			}
 			if (event.key.keysym.sym > SDLK_0 && event.key.keysym.sym < SDLK_7) {
-				if (event.key.keysym.sym == SDLK_1) {speed = levels[0][3]; level_r = levels[0][0]; level_c = levels[0][1];}
-					if (event.key.keysym.sym == SDLK_2) {speed = levels[5][3]; level_r = levels[5][0]; level_c = levels[5][1];}
-						if (event.key.keysym.sym == SDLK_3) {speed = levels[10][3]; level_r = levels[10][0]; level_c = levels[10][1];}
-							if (event.key.keysym.sym == SDLK_4) {speed = levels[15][3]; level_r = levels[15][0]; level_c = levels[15][1];}
-								if (event.key.keysym.sym == SDLK_5) {speed = levels[20][3]; level_r = levels[20][0]; level_c = levels[20][1];}
-									if (event.key.keysym.sym == SDLK_6) {speed = levels[25][3]; level_r = levels[25][0]; level_c = levels[25][1];}
+				if (event.key.keysym.sym == SDLK_1) {reset(); speed = levels[0][3]; level_r = levels[0][0]; level_c = levels[0][1];}
+					if (event.key.keysym.sym == SDLK_2) {reset(); speed = levels[5][3]; level_r = levels[5][0]; level_c = levels[4][1];}
+						if (event.key.keysym.sym == SDLK_3) {reset(); speed = levels[10][3]; level_r = levels[10][0]; level_c = levels[9][1];}
+							if (event.key.keysym.sym == SDLK_4) {reset(); speed = levels[15][3]; level_r = levels[15][0]; level_c = levels[14][1];}
+								if (event.key.keysym.sym == SDLK_5) {reset(); speed = levels[20][3]; level_r = levels[20][0]; level_c = levels[19][1];}
+									if (event.key.keysym.sym == SDLK_6) {reset(); speed = levels[25][3]; level_r = levels[25][0]; level_c = levels[24][1];}
 				handle_events();			// game levels
 				end_game();
 				Ren_menu();
