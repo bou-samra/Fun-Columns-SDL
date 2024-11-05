2a7a6ce71cff6e71742e01749453b91a95802d8d
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-11-05 15:42:26 +1100

    Update menu.c

--- src/menu.c
+++ src/menu.c
@@ -18,27 +18,52 @@ int menu(void) {
 		SDL_PollEvent(&event);
 		switch (event.type)  {
 			case SDL_KEYDOWN:
-			if (event.key.keysym.sym == SDLK_q) {
-				quit = true;				// quit
+			if (event.key.keysym.sym == SDLK_q) {					// quit
+				quit = true;
 			}
-			if (event.key.keysym.sym == SDLK_a) {
-				Ren_about();				// about
+			if (event.key.keysym.sym == SDLK_a) {					// about
+				Ren_about();
 				check_key();
 				Ren_menu();
 			}
-			if (event.key.keysym.sym == SDLK_i) {
-				Ren_info();				// info (instructions)
+			if (event.key.keysym.sym == SDLK_i) {					// info (instructions)
+				Ren_info();
 				check_key();
 				Ren_menu();
 			}
-			if (event.key.keysym.sym > SDLK_0 && event.key.keysym.sym < SDLK_7) {
-				if (event.key.keysym.sym == SDLK_1) {reset(); speed = levels[0][3]; level_r = levels[0][0]; level_c = levels[0][1];}
-					if (event.key.keysym.sym == SDLK_2) {reset(); speed = levels[5][3]; level_r = levels[5][0]; level_c = levels[4][1];}
-						if (event.key.keysym.sym == SDLK_3) {reset(); speed = levels[10][3]; level_r = levels[10][0]; level_c = levels[9][1];}
-							if (event.key.keysym.sym == SDLK_4) {reset(); speed = levels[15][3]; level_r = levels[15][0]; level_c = levels[14][1];}
-								if (event.key.keysym.sym == SDLK_5) {reset(); speed = levels[20][3]; level_r = levels[20][0]; level_c = levels[19][1];}
-									if (event.key.keysym.sym == SDLK_6) {reset(); speed = levels[25][3]; level_r = levels[25][0]; level_c = levels[24][1];}
-				handle_events();			// game levels
+			if (event.key.keysym.sym > SDLK_0 && event.key.keysym.sym < SDLK_7) {	// game levels
+				reset();
+				if (event.key.keysym.sym == SDLK_1) {
+					speed = levels[0][3];
+					level_r = levels[0][0];
+					level_c = levels[0][1];
+				}
+					if (event.key.keysym.sym == SDLK_2) {
+						speed = levels[5][3];
+						level_r = levels[5][0];
+						level_c = levels[4][1];
+					}
+						if (event.key.keysym.sym == SDLK_3) {
+							speed = levels[10][3];
+							level_r = levels[10][0];
+							level_c = levels[9][1];
+						}
+							if (event.key.keysym.sym == SDLK_4) {
+								speed = levels[15][3];
+								level_r = levels[15][0];
+								level_c = levels[14][1];
+							}
+								if (event.key.keysym.sym == SDLK_5) {
+									speed = levels[20][3];
+									level_r = levels[20][0];
+									level_c = levels[19][1];
+								}
+									if (event.key.keysym.sym == SDLK_6) {
+										speed = levels[25][3];
+										level_r = levels[25][0];
+										level_c = levels[24][1];
+									}
+				handle_events();
 				end_game();
 				Ren_menu();
 			}
