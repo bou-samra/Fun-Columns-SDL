f855e18d0a149653147723bcedbc3545eeb1c761
Jason Bou-Samra <154414066+bou-samra@users.noreply.github.com>
2024-11-08 19:47:38 +1100

    Update hiscore.h

--- src/hiscore.h
+++ src/hiscore.h
@@ -7,12 +7,17 @@ extern SDL_Rect entern_back;	// enter name back ground
 extern SDL_Rect entern_trim;	// enter name trim
 extern SDL_Rect cursor;		// Name entry cursor
 
+int filewrite(void);
+int fileread(void);
 int insert_score(int);		// insert entry into high score list
 int Ren_restart(void);		// restart
 int Ren_gameover(void);		// game over
 int Ren_name(void);		// render name entry
 int end_game(void);
 int Ren_cursor(void);		// render cursor
+
+extern FILE *fptr_r;                                           // pointer to read handle
+extern FILE *fptr_w;
 
 extern int score;
 extern char level_r;					// game level (remaining)
